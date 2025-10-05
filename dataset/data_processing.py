48ee6e717f0f470b1829a7faede2ad25d226fc67
hdj <hudejun2002@gmail.com>
2025-10-06 00:48:21 +0800

    feat(dataset): add dataset hashing functionality
    Add a new function `hash_dataset` to generate hashes for train, validation, test, corpus, and custom tokens files. This function utilizes the `hash_files` utility to create unique identifiers for dataset files, which can be used for caching and version control purposes. The function handles potential TypeError exceptions and returns a dictionary mapping dataset types to their respective hashes.

--- dataset/data_processing.py
+++ dataset/data_processing.py
@@ -1,4 +1,5 @@
 from datasets import load_dataset, DatasetDict
+from utils.utils import hash_files
 
 def gen_dataset(param: dict) -> dict[str, DatasetDict]:
     train_file = param['train_file']
@@ -43,4 +44,37 @@ def gen_dataset(param: dict) -> dict[str, DatasetDict]:
         'test':test_dataset,
         'corpus':corpus_dataset,
         'custom_tokens':custom_tokens_dataset
+        }
+
+def hash_dataset(param: dict):
+    train_file = param['train_file']
+    val_file = param['val_file']
+    try:
+        train_val_hash = hash_files(train_file, val_file)
+    except TypeError:
+        train_val_hash = None
+
+    test_file = param['test_file']
+    try:
+        test_hash = hash_files(test_file)
+    except TypeError:
+        test_hash = None
+
+    corpus_file = param['corpus_file']
+    try:
+        corpus_hash = hash_files(corpus_file)
+    except TypeError:
+        corpus_hash = None
+
+    custom_tokens_file = param['custom_tokens_file']
+    try:
+        custom_tokens_hash = hash_files(custom_tokens_file)
+    except TypeError:
+        custom_tokens_hash = None
+
+    return {
+        'train_val':train_val_hash,
+        'test':test_hash,
+        'corpus':corpus_hash,
+        'custom_tokens':custom_tokens_hash
         }
